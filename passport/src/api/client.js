e895f553f0b33e8b1c2f762026d0ed35314d79d1
parsa <macbookair@parsa-macbook.local>
2025-11-27 13:12:31 +0330

    fix api base url for render

--- passport/src/api/client.js
+++ passport/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE = "https://seylane-vip.onrender.com/api";
+export const API_BASE = "https://seylane-vip.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE,
